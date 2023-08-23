ae7f00359eea5903b6d455d9e0827a871309ddbb
gustavoma <gustavo.macedo@duxus.com.br>
2023-08-22 22:11:51 -0300

    create classe Main

---
+++ src/main/java/com/gustavo/application/Main.java
@@ -0,0 +1,10 @@
+package com.gustavo.application;
+
+import com.gustavo.view.TelaFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        TelaFrame telaFrame = new TelaFrame();
+        telaFrame.setResizable(false);
+    }
+}
