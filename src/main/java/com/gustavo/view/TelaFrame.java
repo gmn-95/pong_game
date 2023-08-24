7d765fafea1780da58b7f0f7f68a66111d5dff11
gustavoma <gustavo.macedo@duxus.com.br>
2023-08-24 01:18:04 -0300

    movendo configs iniciais para dentro de Menu

--- src/main/java/com/gustavo/view/TelaFrame.java
+++ src/main/java/com/gustavo/view/TelaFrame.java
@@ -1,14 +1,10 @@
 package com.gustavo.view;
 
-import com.gustavo.*;
-import com.gustavo.jogadores.Jogadores;
-
 import javax.swing.*;
 import java.awt.*;
 
 public class TelaFrame extends JFrame {
 
-    private DesenhaNaTela desenhaNaTela;
 
     private final int widthTela = 800;
     private final int heightTela = 600;
@@ -17,18 +13,15 @@ public class TelaFrame extends JFrame {
         super();
         setTitle("Pong-game");
         setSize(widthTela, heightTela);
-        setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBackground(Color.white);
+        setBackground(Color.BLACK);
+        setLocationRelativeTo(null);
 
-        Jogadores jogadores = new Jogadores();
-        Bola bola = new Bola(widthTela, heightTela);
-        Colisao colisao = new Colisao(bola, widthTela, heightTela);
-        Movimento movimento = new Movimento(jogadores, bola, colisao);
-        Pontuacao pontuacao = new Pontuacao(bola, widthTela, heightTela);
-        desenhaNaTela = new DesenhaNaTela(widthTela, heightTela, jogadores, bola, movimento, pontuacao);
+        Menu menu = new Menu(widthTela, heightTela);
+        add(menu);
 
-        add(desenhaNaTela);
+        setVisible(true);
+
     }
 
 }
