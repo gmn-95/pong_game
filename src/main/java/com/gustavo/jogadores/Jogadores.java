63cdcf46414b71d1d14fdea3db8011a8340c1407
gustavoma <gustavo.macedo@duxus.com.br>
2023-08-24 01:19:44 -0300

    resetando e desenhando jogadores

--- src/main/java/com/gustavo/jogadores/Jogadores.java
+++ src/main/java/com/gustavo/jogadores/Jogadores.java
@@ -18,6 +18,25 @@ public class Jogadores{
         player2 = new Rectangle(760, y, width, height);
     }
 
+    public void draw(Graphics g){
+        g.setColor(Color.WHITE);
+        g.fillRect(getPlayer1().x, getPlayer1().y, getPlayer1().width, getPlayer1().height);
+        g.fillRect(getPlayer2().x, getPlayer2().y, getPlayer2().width, getPlayer2().height);
+    }
+
+    public void resetPosicao(){
+        getPlayer1().x = 30;
+        getPlayer1().y = y;
+        getPlayer1().width = width;
+        getPlayer1().height = height;
+
+        getPlayer2().x = 760;
+        getPlayer2().y = y;
+        getPlayer2().width = width;
+        getPlayer2().height = height;
+
+    }
+
     public Rectangle getPlayer1() {
         return player1;
     }
